a633de8eab517302b0f30e0235de0355be951f64
Saurabh Bhatnagar <saurabh.writes@gmail.com>
2023-10-25 16:18:14 -0400

    Pypi (#4)
    * update setup
    
    * Bump version: 0.7.0 → 0.8.0
    
    * setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="logos-shift-client",
-    version="0.7.0",
+    version="0.8.0",
     author="Saurabh Bhatnagar",
     author_email="saurabh@virevol.com",
+    description="Switch your current LLM with a finetuned one automatically, no additional latency",
+    url="https://api.bohita.com",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         "requests",
@@ -16,5 +23,13 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Framework :: AsyncIO",
     ],
+    project_urls={
+        'Homepage': 'https://api.bohita.com',
+        'Repository': 'https://github.com/virevolai/logos-shift-client',
+    }
 )
